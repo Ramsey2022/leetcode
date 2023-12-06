66146f0c9e3eb58d3d5bd19441830ce4a8d5e41a
Ramsey2022 <abovethelotus@yahoo.com>
2023-12-06 12:12:52 -0800

    added readme and merge_strings problem

---
+++ leetcode75/merge_strings_alternately.py
@@ -0,0 +1,24 @@
+"""
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+#Example
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+"""
+
+def mergeAlternately(self, word1: str, word2: str) -> str:
+        result = []
+        i = 0
+        while i < len(word1) or i < len(word2):
+            if i < len(word1):
+                result.append(word1[i])
+            if i < len(word2):
+                result.append(word2[i])
+            i += 1
+        return ''.join(result)
