33aef111f890af3c1a3a5888442941b5cff6a265
Ramsey2022 <abovethelotus@yahoo.com>
2024-01-04 09:43:51 -0800

    completed reverse_word

---
+++ leetcode75/reverse_words_of_string.py
@@ -0,0 +1,25 @@
+"""
+Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+ 
+
+Example 1:
+
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+"""
+# split the string into list
+# reverse list
+# turn list into string
+
+def reverseWords(self, s: str) -> str:
+    s = s.split()
+    s = s[::-1]
+    return ' '.join(s)
+    
